ab42d8f7e103187158bd5b551b9d16266795df33
Mara Timberlake and Steve Zaske <steve@>
2017-06-05 11:43:44 -0700

    refactor js code to include foreach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,12 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var formFields = ["person1","person2", "animal", "exclamation", "verb", "noun"];
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-
+    formFields.forEach(function(formField){
+      var result = $("input#"+formField).val();
+      $("."+formField).text(result);
+    });
+    
     $("#story").show();
 
     event.preventDefault();
